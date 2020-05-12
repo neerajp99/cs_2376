7b0822e5bedde4623c21494b7df36b4781b6d4ef
neerajp99 <neerajp1999@gmail.com>
2020-05-12 22:50:18 +0530

    Update

--- apriori_algorithm/apriori.py
+++ apriori_algorithm/apriori.py
@@ -32,46 +32,55 @@ for line in lines:
 # L(i) is the unwanted sets and C(r) is the candidate itemset
 frequent_items = list()
 frequent_list = list()
-for i in sorted(item_counts):
-	# Check if the item is greater than the threshold
-	if (item_counts[i] >= min_support):
-		# Check if it's not present in the frequent list 
-		if i not in frequent_list:
-			# Add it to the frequent list
-			frequent_items.append(i)
-			frequent_list.append(i)
 
-# Creating a new list 
+print('------Candidate 1-----')
+for key in sorted(item_counts):
+    print(key, " : ", item_counts[key])
+
+
+print(' ###### List Item 1 ######')
+for i in sorted(item_counts):
+    # Check if the item is greater than the threshold
+    if (item_counts[i] >= min_support):
+        print(key, " : ", item_counts[key])
+        # Check if it's not present in the frequent list
+        if i not in frequent_list:
+            # Add it to the frequent list
+            frequent_items.append(i)
+            frequent_list.append(i)
+
+# Creating a new list
 frequent_new_list = list()
 
 while (confident <= len(frequent_list)):
-	 # returns r length subsequences of elements from the input iterable
-	 for k in itertools.combinations(frequent_list, confident):
-	 	for line in lines: 
-	 		line=line.split(',')
-	 		if k not in item_counts:
-	 			item_counts[k] = 0
-	 		# Check if it already exists in the previous sets
-	 		if set(k).issubset(set(line)):
-	 			# If present, increment the value
-	 			item_counts[k] += 1
+    print('#### Candidate', confident, '##### ')
+    # returns r length subsequences of elements from the input iterable
+    for k in itertools.combinations(frequent_list, confident):
+        for line in lines:
+            line = line.split(',')
+            if k not in item_counts:
+                item_counts[k] = 0
+            # Check if it already exists in the previous sets
+            if set(k).issubset(set(line)):
+                # If present, increment the value
+                item_counts[k] += 1
+        print(k, item_counts[k])
 
-	 for k in itertools.combinations(frequent_list, confident):
-	 	if item_counts[k] >= min_support:
-	 		frequent_items.append(k)
-	 		for ss in k:
-	 			if ss not in frequent_new_list:
-	 				frequent_new_list.append(ss)
+    print("\n\n")
+    print('##### List Item', confident, '#### ')
+
+    for k in itertools.combinations(frequent_list, confident):
+        if item_counts[k] >= min_support:
+            print(k, " : ", item_counts[k])
+            frequent_items.append(k)
+            for ss in k:
+                if ss not in frequent_new_list:
+                    frequent_new_list.append(ss)
+
+    frequent_list, frequent_new_list, confident = frequent_new_list, [], confident + 1
 
 
-	 frequent_list, frequent_new_list, confident  = frequent_new_list, [], confident + 1
-	 
-
-
-print ("Most frequent sets are: ", frequent_items)
-
-
-
+print("Most frequent sets are: \n", frequent_items)
 
 
 # Previous approach -> manual works till 3rd phase
@@ -186,14 +195,3 @@ print ("Most frequent sets are: ", frequent_items)
 
 # for entry in sorted_triples:
 #     print('{0}: {1}'.format(entry[0], entry[1]))
-
-
-
-
-
-
-
-
-
-
-
